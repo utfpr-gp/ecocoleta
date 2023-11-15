590b17d3fc3f3f620502e6600ab8186be87b9756
alguipires <alguipires@gmail.com>
2023-11-15 16:51:39 -0300

    config .env, and swap to bd postgres for tests

--- backend/src/main/java/com/ecocoleta/backend/BackendApplication.java
+++ backend/src/main/java/com/ecocoleta/backend/BackendApplication.java
@@ -3,16 +3,24 @@ package com.ecocoleta.backend;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication implements CommandLineRunner {
 
-	@Value("${twilio.account.sid}")
-	private static String secret;
+	@Value("${spring.datasource.url}")
+	private String secret;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-		System.err.println(secret);
+	}
+
+//		TODO remover sout teste
+	@Override
+	public void run(String... args) {
+		// Agora você pode acessar e imprimir o valor de twilioAccountSid
+		System.err.println("Chave é... " + secret);
 	}
 
 }
--- backend/src/main/java/com/ecocoleta/backend/infra/security/SecurityConfigurations.java
+++ backend/src/main/java/com/ecocoleta/backend/infra/security/SecurityConfigurations.java
@@ -1,8 +1,10 @@
 package com.ecocoleta.backend.infra.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -21,6 +23,25 @@ public class SecurityConfigurations {
     @Autowired
     SecurityFilter securityFilter;
 
+/*    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+        httpSecurity.csrf(csrf -> csrf.disable());
+        httpSecurity.securityMatcher(PathRequest.toH2Console());
+        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        httpSecurity.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/hello").hasRole("ADMIN") //apenas usuarios admin
+                        .anyRequest().authenticated() //apenas usuarios autenticados de qualquer role
+//                                .anyRequest().permitAll()
+                );
+        httpSecurity.headers((headers) -> headers.frameOptions((frame) -> frame.sameOrigin()));
+        httpSecurity.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }*/
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
@@ -28,14 +49,14 @@ public class SecurityConfigurations {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                                .requestMatchers("/h2-console/**").permitAll().anyRequest().permitAll()
-//                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-//                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-//                        .requestMatchers(HttpMethod.GET, "/hello").hasRole("ADMIN") //apenas usuarios admin
-//                        .anyRequest().authenticated() //apenas usuarios autenticados de qualquer role
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/hello").hasRole("ADMIN") //apenas usuarios admin
+                        .anyRequest().authenticated() //apenas usuarios autenticados de qualquer role
 //                                .anyRequest().permitAll()
                 )
-//                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
