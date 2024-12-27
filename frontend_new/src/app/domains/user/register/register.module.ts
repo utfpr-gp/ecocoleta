6c7dd68db9b70226f2e21f7c761afcf222e76831
alguipires <alguipires@gmail.com>
2024-12-26 23:18:26 -0300

    ajuste: rota de cadastramento

--- frontend_new/src/app/domains/user/register/register.module.ts
+++ frontend_new/src/app/domains/user/register/register.module.ts
@@ -4,12 +4,18 @@ import { ButtonModule } from 'primeng/button';
 
 import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterComponent } from './register.component';
+import {DropdownModule} from "primeng/dropdown";
+import {InputTextModule} from "primeng/inputtext";
+import {InputTextareaModule} from "primeng/inputtextarea";
 
 @NgModule({
     imports: [
         CommonModule,
         RegisterRoutingModule,
-        ButtonModule
+        ButtonModule,
+        DropdownModule,
+        InputTextModule,
+        InputTextareaModule
     ],
     declarations: [RegisterComponent]
 })
