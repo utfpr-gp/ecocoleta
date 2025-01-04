930f9f739df4161a989afbe646a2ff037fe09e31
alguipires <alguipires@gmail.com>
2025-01-04 19:24:18 -0300

    ajuste: component footer e topbar modificados com botões personalizados

--- frontend_new/src/app/layout/app.footer.component.ts
+++ frontend_new/src/app/layout/app.footer.component.ts
@@ -4,7 +4,8 @@ import {User, UserService} from "../domains/user/user.service";
 
 @Component({
     selector: 'app-footer',
-    templateUrl: './app.footer.component.html'
+    templateUrl: './app.footer.component.html',
+    styleUrl: './app.footer.component.scss'
 })
 export class AppFooterComponent implements OnInit {
     user: User | null = null;
@@ -18,5 +19,7 @@ export class AppFooterComponent implements OnInit {
         this.userService.userSubject.subscribe((user) => {
             this.user = user;
         });
+
+        console.log('foooter this.user', this.user);
     }
 }
