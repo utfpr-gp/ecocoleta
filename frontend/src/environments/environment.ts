a697b946bfe90a96e294ab3d34585877fb6f3879
alguipires <alguipires@gmail.com>
2025-01-31 03:39:52 -0300

    ajuste: environments

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    API_KEY_GOOGLE_MAPS: process.env['API_KEY_GOOGLE_MAPS'] || '',
-    API: process.env['API'] || 'https://default-url.com'
+    API_KEY_GOOGLE_MAPS: '',
+    API: ''
 };
